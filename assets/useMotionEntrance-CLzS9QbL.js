6eec30ca334decda78e2bddcc1370f6693ed6c02
DenisPoselyanov <79504703+DenisPoselyanov@users.noreply.github.com>
2026-09-23 09:33:09 +0000

    Deploying to gh-pages from @ DenisPoselyanov/biblegames_bot@6389c457e23ee7f4ae3d3e237624653916c24b90 🚀

---
+++ assets/useMotionEntrance-CLzS9QbL.js
@@ -0,0 +1,1 @@
+import{fr as e,ur as t}from"./index-D7qb-Vpu.js";var n=e(t(),1),r=`motion-seen:`;function i(e){let t=`${r}${e}`,[i]=(0,n.useState)(()=>{try{return sessionStorage.getItem(t)!==`1`}catch{return!0}});return(0,n.useEffect)(()=>{try{sessionStorage.setItem(t,`1`)}catch{}},[t]),{shouldEnter:i}}export{i as t};
